d9d496bf258076a2c4d48a5fb61afbb27a3405be
Min Kwon <adamk77@gmail.com>
2012-07-17 17:51:07 -0400

    Add trajectory to wheel

--- SwingerLevelDesigner/Wheel.m
+++ SwingerLevelDesigner/Wheel.m
@@ -7,6 +7,7 @@
 //
 
 #import "Wheel.h"
+#import "Constants.h"
 
 @implementation Wheel
 @synthesize speed;
@@ -47,6 +48,41 @@
     [super loadFromDict:level];
     
     self.speed = [[level objectForKey:@"SpinRate"] floatValue];
+}
+
+- (void) draw:(CGContextRef)ctx {        
+    [[NSColor blackColor] set];
+    
+    float angle = (45) * (M_PI/180.f);
+    
+    float x0 = (self.position.x + 150)/PTM_RATIO;
+    float y0 = (self.position.y + self.size.height/2 + 30)/PTM_RATIO;
+    float v01 = self.speed*10 + 4 + [self getWindForce:1].x;
+    float v02 = self.speed*10 + 4 + [self getWindForce:1].y;
+    float g = 30.0f;
+    
+    float v0x = v01 * cosf(angle);
+    float v0y = v02 * sinf(angle);
+    
+    float range = (2*(v0x*v0y))/g + 2;
+    float t = 0;
+    float stepAmt = v01/400.0f;
+    
+    while (true) {
+        float xPos = (x0 + (cosf(angle)*v01*t)) * PTM_RATIO;
+        float yPos = (y0 + ((sinf(angle)*v02*t) - (g/2)*(t*t))) * PTM_RATIO;
+        
+        t += stepAmt;
+        
+        if (xPos > (x0 + range)*PTM_RATIO) {
+            break;
+        }
+        
+        CGContextFillRect(ctx, CGRectMake(xPos, yPos, 5, 5));
+    }
+    
+    
+    [super draw:ctx];
 }
 
 - (NSString*) gameObjectTypeString {
